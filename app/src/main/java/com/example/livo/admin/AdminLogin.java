0cff7e5b08d68dd57f0d3908625dcdb9bc5a8f78
jeesa <jeesamithsandi@gmail.com>
2024-11-23 23:39:40 +0530

    Admin Login Updates..

--- app/src/main/java/com/example/livo/admin/AdminLogin.java
+++ app/src/main/java/com/example/livo/admin/AdminLogin.java
@@ -1,6 +1,9 @@
 package com.example.livo.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +11,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.livo.LoginActivity;
+import com.example.livo.MainActivity;
 import com.example.livo.R;
+import com.example.livo.databinding.ActivityLoginBinding;
 
 public class AdminLogin extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    admin_dbHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        databaseHelper = new admin_dbHelper(this);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.loginEmail.getText().toString();
+                String password = binding.loginPassword.getText().toString();
+
+                if (email.equals("") || password.equals(""))
+                    Toast.makeText(AdminLogin.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+
+                    if (checkCredentials == true) {
+                        Toast.makeText(AdminLogin.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(AdminLogin.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
         });
+//        binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(AdminLogin.this, adminsignup.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
     }
+
 }
