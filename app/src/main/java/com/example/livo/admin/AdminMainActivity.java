6d531350c8de77ab504257ff5d1cefffb8d22035
Nusrath cassim <154523455+NusrathCassim@users.noreply.github.com>
2024-11-24 16:42:05 +0530

    Admin Main Update

--- app/src/main/java/com/example/livo/admin/AdminMainActivity.java
+++ app/src/main/java/com/example/livo/admin/AdminMainActivity.java
@@ -2,11 +2,7 @@ package com.example.livo.admin;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,21 +12,20 @@ import com.example.livo.admin.adminFragments.AdminHomeFragment;
 import com.example.livo.admin.adminFragments.AdminNotificationFragment;
 import com.example.livo.admin.adminFragments.AdminViewCompanyFragment;
 import com.example.livo.admin.adminFragments.AdminViewCustomerFragment;
-import com.example.livo.customer.HomeFragment;
-import com.example.livo.customer.ProfileFragment;
+import com.example.livo.company.CompanyHomeFragment;
 import com.example.livo.databinding.ActivityAdminMainBinding;
-import com.example.livo.databinding.ActivityCustomerHomeBinding;
 
 public class AdminMainActivity extends AppCompatActivity {
 
     ActivityAdminMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         binding = ActivityAdminMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        replaceFragment(new AdminHomeFragment()); // Default fragment
 
         binding.bottomNavigationViewAdmin.setOnItemSelectedListener(item -> {
             Fragment fragment = new Fragment();
@@ -42,20 +37,18 @@ public class AdminMainActivity extends AppCompatActivity {
             } else if (itemId == R.id.customer) {
                 fragment = new AdminViewCustomerFragment();
             } else if (itemId == R.id.notify) {
-               fragment = new AdminNotificationFragment();
-               } else {
-                fragment = new HomeFragment();
+                fragment = new AdminNotificationFragment();
             }
             replaceFragment(fragment);
             return true;
         });
     }
+
+
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.replace(R.id.admin_frame_layout, fragment);
         fragmentTransaction.commit();
     }
-
-
 }
